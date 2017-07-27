9cb71591faa294cabdd843b9a16164712e98b6e6
tatsukiendo <endotatsuki@tatsuki.local>
2017-07-28 00:09:52 +0900

    createの実装

--- app/controllers/postimages_controller.rb
+++ app/controllers/postimages_controller.rb
@@ -8,4 +8,17 @@ class PostimagesController < ApplicationController
 		@postimage = PostImage.new
 	end
 
+	def create
+		@postimage = PostImage.new(postimage_params)
+	end
+
+
+	private
+
+	def postimage_params
+		params.require(:postimage).permit(:shop_name,
+										  :image_url,
+										  :caption,
+										  :user_id)
+	end
 end
