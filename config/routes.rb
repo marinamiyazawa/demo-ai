aad34c2bfbb03aa44231b2accc7ac80a60f1da2a
tatsukiendo <endotatsuki@tatsuki.local>
2017-08-17 11:25:26 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :postimages, only: [:new, :create, :index] do
   	resource :favorites, only: [:create, :destroy]
   end
+
+  root 'postimages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
