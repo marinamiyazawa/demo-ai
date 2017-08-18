784774aa9ee3b914e9a648131f02f468a30fea99
tatsukiendo <endotatsuki@tatsuki.local>
2017-08-18 16:10:36 +0900

    colmun change

--- db/migrate/20170724091741_devise_create_users.rb
+++ db/migrate/20170724091741_devise_create_users.rb
@@ -31,7 +31,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       # t.datetime :locked_at
 
       t.string :name
-      t.text :profile_image_url
+      t.text :profile_image_id
 
       t.timestamps null: false
     end
