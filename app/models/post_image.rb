8cecc9fec638184f01199b6186b086d8c3ab8c86
tatsukiendo <endotatsuki@tatsuki.local>
2017-08-21 20:45:00 +0900

    validation

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,9 @@ class PostImage < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 	has_many :post_comments, dependent: :destroy
 
+	validates :shop_name, presence: true
+	validates :image_id, presence: true
+
 	def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
     end
