b4eee4fa51fc6d2cdb056aeab3d68ac6897e8d68
tatsukiendo <endotatsuki@tatsuki.local>
2017-08-31 20:18:18 +0900

    インデント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image])
   end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,8 @@
 class FavoritesController < ApplicationController
+
 	def create
 		post_image = PostImage.find(params[:post_image_id])
         favorite = current_user.favorites.new(post_image_id: post_image.id)
-        # favorite = Favorite.new
-        # favorite.user_id = current_user.id
-        # favorite.post_image_id = post_image.id
         favorite.save
         redirect_to post_images_path
 	end
@@ -14,4 +12,5 @@ class FavoritesController < ApplicationController
         favorite.destroy
         redirect_to post_images_path
 	end
+	
 end
