df5959282bcb5373f20bc4794eafd034dd0af9b2
mckeypiko <ko.fujimaki@gmail.com>
2019-12-26 16:35:41 +0900

    change to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_11_19_101045) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_image_id"
